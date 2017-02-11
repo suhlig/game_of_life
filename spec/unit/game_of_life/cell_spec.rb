c7c40d2f1d374e5112d9d27dbe548bae0512a7bb
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-02-11 17:10:54 +0100

    Use subject instead of instance variable

--- spec/unit/game_of_life/cell_spec.rb
+++ spec/unit/game_of_life/cell_spec.rb
@@ -3,29 +3,25 @@ require 'spec_helper'
 
 module GameOfLife
   describe Cell do
-    before(:each) do
-      @cell = Cell.new([1, 1])
-    end
+    subject(:cell) { Cell.new([1, 1]) }
 
     it 'should respond with a position of 1,1' do
-      expect(@cell.position).to eq [1, 1]
+      expect(cell.position).to eq [1, 1]
     end
 
     it 'should be alive as a default' do
-      expect(@cell.state).to eq :live
+      expect(cell.state).to eq :live
     end
 
     it 'should respond as alive when it is alive' do
-      expect(@cell.alive?).to be true
+      expect(cell.alive?).to be true
     end
 
-    describe '#neighbor_coordinates' do
-      it 'should calculate the coordinates of its 8 neighbors properly' do
-        neighbors = [[0, 0], [0, 1], [0, 2],
-                     [1, 0], [1, 2],
-                     [2, 0], [2, 1], [2, 2]]
-        expect(@cell.neighbor_coordinates).to eq neighbors
-      end
+    it 'should calculate the coordinates of its 8 neighbors properly' do
+      neighbors = [[0, 0], [0, 1], [0, 2],
+                   [1, 0],         [1, 2],
+                   [2, 0], [2, 1], [2, 2]]
+      expect(cell.neighbor_coordinates).to eq neighbors
     end
   end
 end
