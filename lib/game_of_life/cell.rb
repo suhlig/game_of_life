21845c4b46d5df7ef17ce177d828fafeca3216ab
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-02-12 13:53:32 +0100

    Make calculate_neighbor_coordinates private

--- lib/game_of_life/cell.rb
+++ lib/game_of_life/cell.rb
@@ -13,13 +13,13 @@ module GameOfLife
       @state == :live
     end
 
+    private
+
     def calculate_neighbor_coordinates
       offset.reduce([]) do |new_arr, coord|
         new_arr << [coord[0] + @position[0], coord[1] + @position[1]]
       end
     end
-
-    private
 
     def offset
       offset_range = (-1..1).to_a
